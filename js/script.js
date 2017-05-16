27effdcdc414e4000e1a1041aa447d65538bff5d
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-05-16 13:08:29 -0400

    se crean el checkbox, corazon y trash

--- js/script.js
+++ js/script.js
@@ -12,7 +12,17 @@ function agregar(){
 	nuevasTareas.appendChild(elementoContenedor);
 	cont.appendChild(nuevasTareas);
 
+	var chck = document.createElement("input");
+	chck.type = "checkbox";
+	chck.setAttribute("class","check");
+	var basura = document.createElement("span");
+	basura.classList.add("fa","fa-trash-o");
+	var cora = document.createElement("span");
+	cora.classList.add("fa","fa-heart");
 
+	nuevasTareas.appendChild(chck);
+	nuevasTareas.appendChild(basura);
+	nuevasTareas.appendChild(cora);
 
 	
 }
