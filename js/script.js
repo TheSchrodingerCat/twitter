f8304de24d8b83a31a1912e03be549c1f0c2da90
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-05-16 13:04:05 -0400

    se crean el div y el span y se asigna un hijo a cada padre

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 function agregar(){
+	var tareas = document.getElementById("tarea").value;
+	document.getElementById("tarea").value = "";
+
+	var cont = document.getElementById("contenedor");
+
+	var nuevasTareas = document.createElement("div");
+	var textoNuevaTarea = document.createTextNode(tareas);
+	var elementoContenedor = document.createElement("span");
+
+	elementoContenedor.appendChild(textoNuevaTarea);
+	nuevasTareas.appendChild(elementoContenedor);
+	cont.appendChild(nuevasTareas);
+
+
+
 	
 }
 
